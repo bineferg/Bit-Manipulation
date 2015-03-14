cca6dd036896e9241fedbc8c0cfc9eac6b1d22de
bineferg <bineferg@users.noreply.github.com>
2015-03-14 01:15:48 -0400

    Create getnum.h

---
+++ getnum.h
@@ -0,0 +1,24 @@
+//Eva Fineberg 
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+
+#ifndef BITS_H
+#define BITS_H
+
+
+long getnum(void);
+void scoot(void);
+
+extern int negFlag;
+extern int isValid;
+extern int isEOF;
+
+//might need to declare definitions for external variables, 
+//maybe true and false to be 1 and 0? IDk
+
+#endif
